a84be5fd24cf3e34bc0d35ae67188b4a6ff0cf46
vivrinailUCA <Vivien.DUFOUR@etu.uca.fr>
2026-01-15 14:30:47 +0100

    ready to use Plugin

---
+++ src/main/java/com/example/plugin/ExampleCommand.java
@@ -0,0 +1,25 @@
+package com.example.plugin;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.hypixel.hytale.server.core.util.EventTitleUtil;
+
+import javax.annotation.Nonnull;
+
+public class ExampleCommand extends AbstractPlayerCommand {
+
+    public ExampleCommand(@Nonnull String name, @Nonnull String description, boolean requiresConfirmation) {
+        super(name, description, requiresConfirmation);
+    }
+
+    @Override
+    protected void execute(@Nonnull CommandContext commandContext, @Nonnull Store<EntityStore> store, @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world) {
+        EventTitleUtil.showEventTitleToPlayer(playerRef, Message.raw("Welcome to the server " + playerRef.getUsername()), Message.raw("Welcome"), true);
+    }
+}
---
+++ src/main/java/com/example/plugin/ExamplePlugin.java
@@ -0,0 +1,19 @@
+package com.example.plugin;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class ExamplePlugin extends JavaPlugin {
+
+    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+        this.getCommandRegistry().registerCommand(new ExampleCommand("welcome", "Command to welcome a player", false));
+    }
+}
